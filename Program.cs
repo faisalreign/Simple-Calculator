4dc77e06b3d974346e56924402af6bfb6449217f
faisalreign <68537434+faisalreign@users.noreply.github.com>
2020-09-29 11:57:00 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculator_by_Faisal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the First value = ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter The Operator = ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter the Second value = ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if(op=="+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "%")
+            {
+                Console.WriteLine(num1 % num2);
+            }
+            else if (op == ">")
+            {
+                Console.WriteLine(num1 > num2);
+            }
+            else if (op == "<")
+            {
+                Console.WriteLine(num1 < num2);
+            }
+            else
+            {
+                Console.WriteLine("You Enter Invalid Operator...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
